17570dfe5b47d808dc5acffcfbe2b23c8508e035
gyim1345 <gyim1345@gmail.com>
2020-01-20 00:23:44 +0900

    Edit webpack.config.js
    Edit for optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,16 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
 
 module.exports = {
   // TODO: 환경변수 NODE_ENV에 따라 development나 production 값을 설정하세요 
-  mode: "development",
+  mode,
   entry: {
     main: "./src/app.js"
   },
@@ -57,21 +63,44 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: mode === "development" ? "(개발용)" : ""
       },
       minify:
-        process.env.NODE_ENV === "production"
+        mode === "production"
           ? {
               collapseWhitespace: true, // 빈칸 제거
               removeComments: true // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production"
+        hash: mode === "production"
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
+    new CopyPlugin([
+      {
+        from: "./node_modules/axios/dist/axios.min.js",
+        to: "./axios.min.js" // 목적지 파일에 들어간다
+      }
+    ]),
+    ...(mode === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
       : [])
-  ]
-  // TODO: 여기에 최적화 설정을 구성하세요 
+  ],
+  optimization: {
+    minimizer:
+      mode === "production"
+        ? [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true
+                }
+              }
+            })
+          ]
+        : []
+  },
+  externals: {
+    axios: "axios"
+  }
 };
