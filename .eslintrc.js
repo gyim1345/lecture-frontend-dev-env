3f815ad07c17cdb36953eacab42ae88a68f83c9d
gyim1345 <gyim1345@gmail.com>
2020-01-20 00:13:53 +0900

    Edit .eslintrc.js
    Edit to add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: "eslint:recommended",
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly"
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module"
     },
-    // TODO: 프리티어 설정을 추가하세요.
-    "rules": {
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error"
     }
-};
+  };
+  
